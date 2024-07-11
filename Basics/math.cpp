cb016c2c86dd1d545d61ecaef79839bf9e2faef5
Sunny <sobhit230@gmail.com>
2024-07-11 13:00:26 +0530

    Learning

--- Basics/math.cpp
+++ Basics/math.cpp
@@ -21,6 +21,7 @@ int reverseInt(int x)
     return ans;
 }
 
+// gcd and lcm
 vector<long long> lcmAndGcd(long long a, long long b)
 {
     // code here
@@ -43,6 +44,58 @@ vector<long long> lcmAndGcd(long long a, long long b)
     ans.push_back(lcm);
     ans.push_back(gcd);
     return ans;
+}
+
+bool isArmstrong(int num)
+{
+    // Calculate the number of
+    // digits in the given number
+    int k = to_string(num).length();
+    // Initialize the sum of digits
+    // raised to the power of k to 0
+    int sum = 0;
+    // Copy the value of the input
+    // number to a temporary variable n
+    int n = num;
+    // Iterate through each
+    // digit of the number
+    while (n > 0)
+    {
+        // Extract the last
+        // digit of the number
+        int ld = n % 10;
+        // Add the digit raised to
+        // the power of k to the sum
+        sum += pow(ld, k);
+        // Remove the last digit
+        // from the number
+        n = n / 10;
+    }
+    // Check if the sum of digits raised to
+    // the power of k equals the original number
+    return sum == num ? true : false;
+}
+
+// sum of all divisors from 1 to n
+int sumOfdDivisors(int i)
+{
+    int sum = 0;
+    for(int j = 1;j*j<=i;++j){
+        if(i%j == 0){
+            sum += j ;
+            if(j!=i/j){
+                sum += i / j;
+            }
+        }
+    }
+}
+
+long long sumofDivisors(int N){
+    long long totalSum = 0;
+    for(int i = 1; i<=N;++i){
+        totalSum += sumOfdDivisors(i);
+    }
+    return totalSum;
 }
 
 int main()
